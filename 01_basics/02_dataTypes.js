1b29b19674c353ccd94be4b7a1deb13808322a1d
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-08-19 05:17:30 +0000

    JS dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";       // treat all JS code newer version's
+
+// alert(3+3)       // we are using nodejs, not browser
+
+console.log( 3 + 3);
+
+let name = "Sidhant"
+let age = 24
+let isLoggedIn = false
+let state;
+
+console.log(name);
+
+// number => 2 to the power 56
+// bigInt
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Sidhant");
+console.log(typeof null)            // object
+console.log(typeof undefined)       // undefined
+
