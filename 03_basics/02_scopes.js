efb6753dad9794d90bcade5ca77661d29e78d877
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-08-23 07:56:06 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,12 +4,55 @@ let a = 310
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 
+
+function one() {
+    const username = "sidhant"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);            // error
+
+    two()
+
+}
+
+// one()
+
+
+if (true) {
+    const username = "sidhant"
+    if (username === "sidhant") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);                    // error
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++     Interesting     ++++++++++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num) {
+    return num + 1
+}
+
+
+// addTwo(5)
+
+const addTwo = function (num) {
+    return num + 2
+}
 
