0eee54d4d041b96f82e01798875fedc32796e053
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-08-27 05:21:04 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: "JavaScript",
+    py: "Python",
+    rb: "Ruby",
+    cpp: "C++",
+}
+
+for (const key in myObject) {
+    // console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+const programming = ["js", "py", "java", "rb", "swift"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN',  "India")
+// map.set('USA', "United States of America")
+// map.set('FR',  "France")
+// map.set('SA',  "South Africa")
+// map.set('SPN', "Spain")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
