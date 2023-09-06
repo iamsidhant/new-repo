923c6ec7c746cb2f562f95b8113a26172c2406e2
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-06 17:22:43 +0000

    event basics

--- 08_events/one.js
+++ 08_events/one.js
@@ -9,12 +9,11 @@
 
 // type - returns a string containing event's type
 // timestamp - returns time in milliseconds
-// defaultPrevented- returns a boolean value
+// preventDefault - method tells the user agent that if event does not handled explicitly then its default actions should not be taken as normally would be.
 
 // target - reference to a object onto which element was dispatched
 
-// currentTarget - identifies the current target for the event, as the event traverses the DOM.
-//                   It always refers to the  element to which the event handler has been attached
+// currentTarget - identifies the current target for the event, as the event traverses the DOM. It always refers to the  element to which the event handler has been attached
 
 // clientX - Provides the horizontal coordinate within the application's viewport at which event occured
 // clientY - Provides the vertical coordinate within the application's viewport at which event occured
@@ -29,3 +28,30 @@
 // code - this property returns a value that isn't altered by keyboard layout or the state of the modifier keys
 
 
+// document.getElementById('images').addEventListener('click', function(e) {
+//     console.log("clicked inside the ul");
+// }, false)
+
+// document.getElementById('owl').addEventListener('click', function(e) {
+//     console.log("owl clicked");
+//     e.stopPropagation();
+// }, false)
+
+// document.getElementById('google').addEventListener('click', function(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     console.log("google clicked");
+// }, false)
+
+
+document.querySelector('#images').addEventListener('click', function(e) {
+    console.log(e.target.tagName);
+    if (e.target.tagName === 'IMG') {
+        console.log(e.target.id);
+        let removeIt = e.target.parentNode
+        removeIt.remove()
+    }
+
+})
+
+// removeIt.parentNode.removeChild(removeIt)
