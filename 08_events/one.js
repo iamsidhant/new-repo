978897e1eed3335467ff69f3fe8bed836b62c1f3
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-06 11:34:06 +0000

    Event Properties

---
+++ 08_events/one.js
@@ -0,0 +1,31 @@
+// document.getElementById('owl').onclick = function () {
+//     alert("owl clicked")
+// }
+
+// attachEvent()
+// jQuery - on
+
+// *************    Properties     *************
+
+// type - returns a string containing event's type
+// timestamp - returns time in milliseconds
+// defaultPrevented- returns a boolean value
+
+// target - reference to a object onto which element was dispatched
+
+// currentTarget - identifies the current target for the event, as the event traverses the DOM.
+//                   It always refers to the  element to which the event handler has been attached
+
+// clientX - Provides the horizontal coordinate within the application's viewport at which event occured
+// clientY - Provides the vertical coordinate within the application's viewport at which event occured
+
+// screenX - Provides the horizontal coordinate (offset) of the mouse pointer in global (screen) coordinates 
+// screenY - Provides the vertical coordinate (offset) of the mouse pointer in global (screen) coordinates
+
+
+// altkey - boolean value that indicates whether the alt key was pressed or not when a given mouse event occurs
+// ctrlkey - boolean value that indicates whether the ctrl key was pressed or not when a given mouse event occurs
+// shiftkey - boolean value that indicates whether the shift key was pressed or not when a given mouse event occurs
+// code - this property returns a value that isn't altered by keyboard layout or the state of the modifier keys
+
+
