12ab5aa698ede424581447bffb2a16dc5439ec96
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-19 10:55:52 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -21,3 +21,36 @@ new Promise(function(resolve, reject) {
     console.log("Async 2 resolved");
 })
 
+const promiseThree = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        resolve({username: "Sidhant", email: "sidhant_15@gmail.com"})
+    }, 1000);
+})
+
+promiseThree.then(function(user) {
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "sidhant", password: "123"})
+        } else {
+            reject("ERROR: Something went wrong")
+        }
+    }, 1000);
+})
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+
