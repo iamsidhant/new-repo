caab479a2b48dba934d6b6d0179a55d08b56a2c4
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-22 11:35:48 +0000

    Prototype in JavaScript

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -25,11 +25,46 @@ Array.prototype.heySidhant = function(){
     console.log(`You can deal with any situation`);
 }
 
-myHeroes.Sidhant()
-myHeroes.heySidhant()
-heroPower.Sidhant()
+// myHeroes.Sidhant()
+// myHeroes.heySidhant()
+// heroPower.Sidhant()
 // heroPower.heySidhant()
 
 
 //      Inheritance     //
 
+const User = {
+    name: "Sidhant",
+    email: "compute@gmail.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: "JS Assignment",
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+
+// Modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "GitHub   "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is:  ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"Sidhant".trueLength()
+"Ekam".trueLength()
