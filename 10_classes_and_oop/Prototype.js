c5df4896a611be15658ae0f6fecfb529288853ba
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-22 11:10:59 +0000

    JS Prototype

---
+++ 10_classes_and_oop/Prototype.js
@@ -0,0 +1,35 @@
+// let Myname = "Sidhant   ";
+// let language = "javascript  "
+
+// console.log(Myname.trueLength);
+
+
+let myHeroes = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpidermanPower: function(){
+        console.log(`Spidy Power is ${this.spiderman}`);
+    }
+
+}
+
+
+Object.prototype.Sidhant = function(){
+    console.log(`God is present in every human`);
+}
+
+Array.prototype.heySidhant = function(){
+    console.log(`You can deal with any situation`);
+}
+
+myHeroes.Sidhant()
+myHeroes.heySidhant()
+heroPower.Sidhant()
+// heroPower.heySidhant()
+
+
+//      Inheritance     //
+
