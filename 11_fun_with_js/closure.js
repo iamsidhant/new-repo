b3bda9f5ae887fbc9daedd2fa2fe2797b29caecb
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-25 08:23:06 +0000

    Closure in javascript

--- 11_fun_with_js/closure.js
+++ 11_fun_with_js/closure.js
@@ -29,19 +29,36 @@
 // Lexical Scoping - Inner function has access to scope of outer function but vice-versa is not true. Inner functions not have access to scope of each other function.
 
 
-function makeFunc() {
-    const name = "Mozilla";
-    function displayName() {
-      console.log(name);
-    }
-    return displayName;
-}
+// function makeFunc() {
+//     const name = "Mozilla";
+//     function displayName() {
+//       console.log(name);
+//     }
+//     return displayName;
+// }
   
-const myFunc = makeFunc();
-myFunc();
+// const myFunc = makeFunc();
+// myFunc();
 
 // A closure is the combination of a function and the lexical scope within which that function was declared.
 
 
 
 
+// document.getElementById("orange").onclick = function(){
+//     document.body.style.backgroundColor = "orange"
+// }
+// document.getElementById("green").onclick = function(){
+//     document.body.style.backgroundColor = "green"
+// }
+
+function clickHandler(color){
+    // document.body.style.backgroundColor = `${color}`
+    
+    return function(){
+        document.body.style.backgroundColor = `${color}`
+    }
+}
+
+document.getElementById("orange").onclick = clickHandler("orange")
+document.getElementById("green").onclick = clickHandler("green")
