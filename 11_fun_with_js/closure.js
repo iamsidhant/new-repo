5c8a1f239b89b153c22869c9a7081e0a24e62e62
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-25 07:42:20 +0000

    Closure

---
+++ 11_fun_with_js/closure.js
@@ -0,0 +1,47 @@
+// function init() {
+//     let name = "Mozilla";
+//     function displayName() {
+//         // displayName() is the inner function, that forms the closure
+//         console.log(name);
+//     }
+//     displayName();
+// }
+// init();
+
+// function outer(){
+//     let username = "sidhant"
+//     console.log("OUTER", secret);
+//     function inner(){
+//         let secret = "my123"
+//         console.log("inner", username);
+//     }
+//     function innerTwo(){
+//         console.log("innerTwo", username);
+//         console.log(secret);
+//     }
+//     inner()
+//     innerTwo()
+
+// }
+// outer()
+// console.log("TOO OUTER", username);
+
+// Lexical Scoping - Inner function has access to scope of outer function but vice-versa is not true. Inner functions not have access to scope of each other function.
+
+
+function makeFunc() {
+    const name = "Mozilla";
+    function displayName() {
+      console.log(name);
+    }
+    return displayName;
+}
+  
+const myFunc = makeFunc();
+myFunc();
+
+// A closure is the combination of a function and the lexical scope within which that function was declared.
+
+
+
+
